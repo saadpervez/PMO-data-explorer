220f6d0dfe335c1c652aaae04502a8b0d715a1e6
James Lane <lanejames35@gmail.com>
2020-01-16 16:22:27 -0500

    Test Chartist.js with data in JSON

---
+++ script/src/pmo.js
@@ -0,0 +1,13 @@
+var data = [
+  {
+    "pmoID": 1,
+    "pmoName":"Cannabis Use",
+    "description":"How often cannabis was used in the last year",
+    "estimates": {
+        "bySex": {
+            "labels": [ "Did not use","Once or twice","Three or more times" ],
+            "series": [ { "name": "Durham Region Male", "data": [ 74,5,18] },{ "name": "Durham Region Female", "data": [ 82,7,10] },{ "name": "Ontario Male", "data": [ 79,5,15] },{ "name": "Ontario Female", "data": [ 81,7,11] } ]
+        }
+    }
+  }
+];
