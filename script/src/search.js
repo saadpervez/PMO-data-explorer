c5fca55fb941b7ebc8af21e09b56b8c4f72c685d
James <lanejames35@gmail.com>
2020-01-17 23:27:25 -0500

    all function needed to build an indicator with js

--- script/src/search.js
+++ script/src/search.js
@@ -28,10 +28,101 @@
     var link = document.createElement('a');
     var text = document.createTextNode('#');
     link.setAttribute('class','indicator-permalink');
-    link.setAttribute('href', indicator.pmoName.split(' ').join('-').toLowerCase());
+    link.setAttribute('href', '#' + indicator.pmoName.split(' ').join('-').toLowerCase());
     link.setAttribute('title', 'Link to just this indicator');
     link.appendChild(text);
     block[id].appendChild(link);
+  }
+  function buildArticleHeader(indicator, id){
+    var block = document.querySelectorAll('.indicator-block');
+    var headContainer = document.createElement('div');
+    var header = document.createElement('h3');
+    var desc = document.createElement('p');
+    var ctrlDiv = document.createElement('div');
+    var btnDiv = document.createElement('div');
+    var genderBtn = document.createElement('button');
+    var genderBtnText = document.createTextNode('Sex relative');
+    var gradeBtn = document.createElement('button');
+    var gradeBtnText = document.createTextNode('Grade relative');
+    var allBtn = document.createElement('button');
+    var allBtnText = document.createTextNode('All responses');
+    var optionsHint = document.createElement('a');
+    var optionsHintText = document.createTextNode('?');
+    var headerText = document.createTextNode(indicator.pmoName);
+    var descText = document.createTextNode(indicator.description);
+    headContainer.setAttribute('class', 'indicator-header');
+    header.setAttribute('class', 'indicator-title');
+    header.appendChild(headerText);
+    desc.setAttribute('class', 'indicator-desc');
+    desc.appendChild(descText);
+    headContainer.appendChild(header);
+    headContainer.appendChild(desc);
+    // Buttons
+    ctrlDiv.setAttribute('class', 'view-control');
+    btnDiv.setAttribute('class', 'view-mode-control btn-group-sm btn-group');
+    genderBtn.setAttribute('class', 'btn btn-default');
+    genderBtn.appendChild(genderBtnText);
+    gradeBtn.setAttribute('class', 'btn btn-default');
+    gradeBtn.appendChild(gradeBtnText);
+    allBtn.setAttribute('class', 'btn btn-default');
+    allBtn.appendChild(allBtnText);
+    optionsHint.setAttribute('class', 'btn btn-sm btn-default overlay-lnk');
+    optionsHint.setAttribute('href', '#options-hint');
+    optionsHint.setAttribute('aria-controls', 'options-hint');
+    optionsHint.setAttribute('role', 'button');
+    optionsHint.appendChild(optionsHintText);
+    btnDiv.appendChild(genderBtn);
+    btnDiv.appendChild(gradeBtn);
+    btnDiv.appendChild(allBtn);
+    ctrlDiv.appendChild(btnDiv);
+    ctrlDiv.appendChild(optionsHint)
+    block[id].appendChild(headContainer);
+    block[id].appendChild(ctrlDiv);
+  }
+  function buildArticleCharts(indicator, id){
+    var block = document.querySelectorAll('.indicator-block');
+    var chartContainer = document.createElement('div');
+    chartContainer.setAttribute('class', 'indicator-chart');
+    var chartSex = document.createElement('div');
+    chartSex.setAttribute('id', 'chart-sex');
+    var chartGrade = document.createElement('div');
+    chartGrade.setAttribute('id', 'chart-grade');
+    var chartAll = document.createElement('div');
+    chartAll.setAttribute('id', 'chart-all');
+    var charts = [ chartGrade, chartSex, chartAll ];
+    charts.forEach(function(el){
+      chartContainer.appendChild(el);
+    })
+    block[id].appendChild(chartContainer);
+  }
+  // Note tabs <-- need to be initalized with WET
+  function buildArticleNotes(indicator, id){
+    var block = document.querySelectorAll('.indicator-block');
+    var noteDiv = document.createElement('div');
+    noteDiv.setAttribute('class', 'indicator-footer');
+    var tabsDiv = document.createElement('div');
+    tabsDiv.setAttribute('class', 'wb-tabs');
+    var panelDiv = document.createElement('div');
+    panelDiv.setAttribute('class', 'tabpanels');
+    var tab1 = document.createElement('details');
+    var tab2 = document.createElement('details');
+    var tab3 = document.createElement('details');
+    var tabs = [ tab1, tab2, tab3 ];
+    tabs.forEach(function(el, idx){
+      var tabElement = document.createElement('summary');
+      var tabName = document.createTextNode("Notes");
+      tabElement.appendChild(tabName);
+      var sum = document.createElement('p');
+      var sumText = document.createTextNode('The note text goes here.');
+      sum.appendChild(sumText);
+      el.setAttribute('class', 'tab' + idx);
+      el.appendChild(tabElement);
+      el.appendChild(sum);
+      panelDiv.appendChild(el)
+    });
+    tabsDiv.appendChild(panelDiv);
+    noteDiv.appendChild(tabsDiv);
+    block[id].appendChild(noteDiv);
   }
   search.addEventListener('input', function(e){
     var size = e.target.value.length;
@@ -45,6 +136,9 @@
       results.forEach(function(elem, idx){
         buildArticleNode();
         buildArticleLink(elem, idx);
+        buildArticleHeader(elem,idx);
+        buildArticleCharts(elem, idx);
+        buildArticleNotes(elem, idx);
       });        
       }, 1000);
     }
